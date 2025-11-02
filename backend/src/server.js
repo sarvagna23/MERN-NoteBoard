c111927afc2b2ffcb22fe9eb8dad92f83360ab1e
Sai Sarvagna <saisarvagna@Sais-MacBook-Air.local>
2025-11-02 18:17:18 -0500

    initial commit

---
+++ backend/src/server.js
@@ -0,0 +1,35 @@
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+
+import notesRoutes from './routes/notesRoutes.js';
+import { connectDB } from './config/db.js';
+import rateLimiter from './middleware/rateLimiter.js';
+
+dotenv.config();     
+
+const app = express();
+const PORT = process.env.PORT || 5001;
+
+app.use(cors({
+    origin: "http://localhost:5173",  
+}));
+app.use(express.json());  // Middleware to parse JSON request bodies
+app.use(rateLimiter);
+
+// app.use((req,res,next) => {
+    
+//     console.log(`${req.method} request for '${req.url}'`);
+//     next();
+// });
+app.use("/api/notes", notesRoutes);
+
+connectDB().then(() => {    
+    app.listen (PORT, () => {   
+        console.log(`Server running on port`, PORT);    
+    });
+}
+
+);
+              
+
